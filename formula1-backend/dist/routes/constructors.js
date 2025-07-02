3489335b86c48b1220128479e74b4159f3e0787f
Matias Rondan <matias.rondan@globant.com>
2025-07-02 13:23:41 -0300

    remove dist

--- formula1-backend/dist/routes/constructors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const constructorsController_1 = require("../controllers/constructorsController");
-const router = (0, express_1.Router)();
-router.get('/standings', constructorsController_1.getConstructorStandigs);
-exports.default = router;
--- formula1-backend/dist/routes/drivers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const driversController_1 = require("../controllers/driversController");
-const router = (0, express_1.Router)();
-router.get('/standings', driversController_1.getCurrentDriverStandings);
-exports.default = router;
